bc70ea9486c052f500a2e8955e9aa6626a516747
bag1s3k <jetam.mravenec@gmail.com>
2026-01-14 20:58:44 +0100

    Add example with async and sync usage

--- frankfurter_api/test.py
+++ frankfurter_api/test.py
@@ -1,16 +1,79 @@
 ﻿import requests
+import httpx
+import asyncio
+import time
 
-from main import Currency
+from main import CurrencyAPI
 
-currency = Currency()
+API_EXAMPLES = (
+    {
 
-def get_currency():
-    response = requests.get(tmp := currency.specific_crcs(["CZK", "USD"]))
-    print(tmp)
+    },
+    {
+        "base": "CZK"
+    },
+    {
+        "symbols": ["USD", "GBP"]
+    },
+    {
+        "base": "USD",
+        "symbols": ["CZK", "JPY"]
+    },
+    {
+        "date": "2024-12-24"
+    },
+    {
+        "date": "2023-01-01",
+        "base": "CHF"
+    },
+    {
+        "date": "2022-10-01",
+        "symbols": ["CZK", "PLN"]
+    },
+    {
+        "date": "2025-10-01..2025-10-10"
+    },
+    {
+        "date": "2024-06-01..2024-06-05",
+        "base": "EUR",
+        "symbols": ["CZK"]
+    },
+    {
+        "date": "2023-11-01..2023-11-30",
+        "base": "GBP",
+        "symbols": ["USD", "CAD", "AUD"]
+    }
+)
+
+currency = CurrencyAPI()
+
+async def get_currency(client, kwargs, index):
+    response = await client.get(currency.get_url(**kwargs))
     if response.status_code == 200:
-        return response.json()
+        print(f"Successful response with asyncio: {index}")
     else:
-        raise Exception("Invalid input")
+        raise Exception("Something failed")
 
-if currency_info := get_currency():
-    print(currency_info)
+async def main():
+    async with httpx.AsyncClient() as client:
+        async with asyncio.TaskGroup() as tg:
+            for index, requirements in enumerate(API_EXAMPLES):
+                tg.create_task(get_currency(client, requirements, index))
+
+if "__main__" == __name__:
+    t1_sync = time.time()
+    for i, requirements_sync in enumerate(API_EXAMPLES):
+        response_sync = requests.get(currency.get_url(**requirements_sync))
+        if response_sync.status_code == 200:
+            print(f"Successful response without asyncio: {i}")
+        else:
+            raise Exception("Something failed")
+    time_sync = time.time() - t1_sync
+    print()
+
+    t1_async = time.time()
+    asyncio.run(main())
+    time_async = time.time() - t1_async
+    print(f"Without asyncio: {time_sync:.2f}")
+    print(f"With asyncio: {time_async:.2f}\n")
+    print(f"With asyncio is {round((time_async * time_sync * 100), 0)}% more faster")

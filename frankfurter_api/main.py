4b1455fb00ec2fd7361fb35fa2a9ff9ae1d30efd
bag1s3k <jetam.mravenec@gmail.com>
2026-01-14 12:45:19 +0100

    Create CRC class
    Add specific CRC func
    Test file for usage

---
+++ frankfurter_api/main.py
@@ -0,0 +1,25 @@
+﻿BASE_API_URL = "https://api.frankfurter.dev/v1"
+
+
+class Currency:
+
+    def __init__(self, base=None, to=None, amount=None, crc=None):
+        self.base = base
+        self.to = to
+        self.amount = amount
+        self.crc= crc
+
+    def specific_crc(self, crc_override=None):
+        """ Generates the API endpoint URL with an optional CRC override.
+            :param crc_override: CRC to override
+            :return: complete formatted API URL string"""
+        crc = crc_override or self.crc
+
+        if crc:
+            return f"{BASE_API_URL}/latest?base={crc}"
+        else:
+            return f"{BASE_API_URL}/latest"
+
+if "__main__" == __name__:
+    currency = Currency()
+    print(currency.specific_crc())
---
+++ frankfurter_api/test.py
@@ -0,0 +1,16 @@
+﻿import requests
+
+from main import Currency
+
+currency = Currency()
+
+def get_currency():
+    response = requests.get(tmp := currency.specific_crc("CZK"))
+    print(tmp)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+if currency_info := get_currency():
+    print(currency_info)

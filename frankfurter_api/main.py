5a84c14bc14ce11573d9f2a812fd4248b4d59305
bag1s3k <jetam.mravenec@gmail.com>
2026-01-14 21:19:32 +0100

    Add dynamic list of supported currencies

--- frankfurter_api/main.py
+++ frankfurter_api/main.py
@@ -1,48 +1,30 @@
 ﻿from datetime import datetime
 from typing import Optional, Iterable
 
-SUPPORTED_CURRENCIES = {
-            "AUD": "Australian Dollar",
-            "BRL": "Brazilian Real",
-            "CAD": "Canadian Dollar",
-            "CHF": "Swiss Franc",
-            "CNY": "Chinese Yuan",
-            "CZK": "Czech Koruna",
-            "DKK": "Danish Krone",
-            "EUR": "Euro",
-            "GBP": "British Pound",
-            "HKD": "Hong Kong Dollar",
-            "HUF": "Hungarian Forint",
-            "IDR": "Indonesian Rupiah",
-            "ILS": "Israeli New Shekel",
-            "INR": "Indian Rupee",
-            "ISK": "Icelandic Króna",
-            "JPY": "Japanese Yen",
-            "KRW": "South Korean Won",
-            "MXN": "Mexican Peso",
-            "MYR": "Malaysian Ringgit",
-            "NOK": "Norwegian Krone",
-            "NZD": "New Zealand Dollar",
-            "PHP": "Philippine Peso",
-            "PLN": "Polish Zloty",
-            "RON": "Romanian Leu",
-            "SEK": "Swedish Krona",
-            "SGD": "Singapore Dollar",
-            "THB": "Thai Baht",
-            "TRY": "Turkish Lira",
-            "USD": "United States Dollar",
-            "ZAR": "South African Rand"
-        }
+import requests
+
 DATE_PATTERN = "%Y-%m-%d"
 
 class CurrencyValidationError(ValueError):
     ...
 
-class DateValidationError(ValueError):
-    ...
-
 class CurrencyAPI:
     BASE_URL = "https://api.frankfurter.dev/v1"
+
+    def __init__(self):
+        self.supported_currencies = self.get_supported_currencies()
+
+    def get_supported_currencies(self):
+        try:
+            response = requests.get(f"{self.BASE_URL}/currencies")
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.HTTPError as e:
+            status_code = e.response.status_code
+            raise CurrencyValidationError(f"API returned error status: {status_code}. Details: {e}")
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Network or connection error: {e}")
+
 
     def get_url(self, base: Optional[str] = None, symbols: Optional[Iterable[str]] = None, date: Optional[str] = None) -> str:
         """Generates the API endpoint URL"""
@@ -69,14 +51,15 @@ class CurrencyAPI:
 
     def validate_currencies(self, currencies: Iterable[str]):
         """Check if CRC or CRCS are being supported"""
-        invalid = [c for c in currencies if c not in SUPPORTED_CURRENCIES]
+        invalid = [c for c in currencies if c not in self.supported_currencies]
         if invalid:
             raise CurrencyValidationError(f"Unsupported currency codes: {",".join(invalid)}")
 
-    def validate_date(self, date: str):
+    @staticmethod
+    def validate_date(date: str):
         """Check if date is in form 1999-01-04"""
         try:
             for d in date.split(".."):
                 datetime.strptime(d, DATE_PATTERN)
         except ValueError:
-            raise DateValidationError(f"Invalid date: {", ".join(date)}")
+            raise CurrencyValidationError(f"Invalid date: {", ".join(date)}")

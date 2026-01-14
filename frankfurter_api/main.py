ce74633e44af5e8ba456454c769b05f8ef9efb71
bag1s3k <jetam.mravenec@gmail.com>
2026-01-14 13:14:39 +0100

    Add specific CRCS func

--- frankfurter_api/main.py
+++ frankfurter_api/main.py
@@ -1,24 +1,34 @@
-﻿BASE_API_URL = "https://api.frankfurter.dev/v1"
+﻿from typing import Optional
 
 
 class Currency:
 
-    def __init__(self, base=None, to=None, amount=None, crc=None):
+    def __init__(self, base=None, to=None, amount=None, crc=None, crcs=None):
         self.base = base
         self.to = to
         self.amount = amount
-        self.crc= crc
+        self.crc = crc
+        self.crcs = crcs
+        self.BASE_API_URL = "https://api.frankfurter.dev/v1"
 
     def specific_crc(self, crc_override=None):
-        """ Generates the API endpoint URL with an optional CRC override.
-            :param crc_override: CRC to override
-            :return: complete formatted API URL string"""
+        """Generates the API endpoint URL with an optional CRC override."""
         crc = crc_override or self.crc
 
         if crc:
-            return f"{BASE_API_URL}/latest?base={crc}"
+            return f"{self.BASE_API_URL}/latest?base={crc}"
         else:
-            return f"{BASE_API_URL}/latest"
+            return f"{self.BASE_API_URL}/latest"
+
+
+    def specific_crcs(self, crcs_override: Optional[list[str]] = None):
+        """Generates the API endpoint URL with an optional CRCS override"""
+
+        crcs = crcs_override or self.crcs
+        if crcs:
+            return f"{self.BASE_API_URL}/latest?symbols={",".join(crc for crc in crcs)}"
+        else:
+            return f"{self.BASE_API_URL}/latest"
 
 if "__main__" == __name__:
     currency = Currency()
--- frankfurter_api/test.py
+++ frankfurter_api/test.py
@@ -5,12 +5,12 @@ from main import Currency
 currency = Currency()
 
 def get_currency():
-    response = requests.get(tmp := currency.specific_crc("CZK"))
+    response = requests.get(tmp := currency.specific_crcs(["CZK", "USD"]))
     print(tmp)
     if response.status_code == 200:
         return response.json()
     else:
-        return None
+        raise Exception("Invalid input")
 
 if currency_info := get_currency():
     print(currency_info)
